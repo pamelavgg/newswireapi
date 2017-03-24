33fa0a175f08efa3c6d45e36d7fd3d2384b4228a
Pamela González <pamelavgg@Pamelas-MacBook-Air.local>
2017-03-24 00:05:36 -0600

    Add minor changes

--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from nyt_request import transform_hours, request
 
 def main():
     date = '2017/01/01'
-    response = request('51505e1b122b4e04953306674b56797e', 'business day', transform_hours(date))
+    api_key = '51505e1b122b4e04953306674b56797e'
+    section = 'business day'
+
+    request(api_key, section, transform_hours(date))
 
 
 if __name__ == '__main__':
